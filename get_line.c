d700b5f30d6a0fb0a29fe63090af4a6d75daa30e
hassanzali <hassanzali622@gmail.com>
2023-11-06 16:07:24 -0800

    changes

---
+++ get_line.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+/**
+*ssize_t get_line - reads buffer from the stream
+*@lineptr: pointer to the line
+*@n: parameter for number
+*@strem: array for the buffer
+*
+*Return: return number of characters or - 1 when fail.
+*/
+ssize_t get_line(char **lineptr, size_t *n, FILE *stream)
+{
+	static char buf[MAX_BUFFER_SIZE];
+	static size_t buf_index = (0);
+	static ssize_t bytes_read = (0);
+	size_t line_length = (0);
+	char *line = *lineptr;
+
+	if (!lineptr || !n || !stream)
+	return (-1);
+
+	if (*n == 0)
+	*n = 100;
+
+	if (bytes_read <= 0)
+	{
+		bytes_read = read(fileno(stream), buf, MAX_BUFFER_SIZE);
+
+	if (bytes_read < 0)
+	return (-1);
+
+	else if (bytes_read == 0)
+	return (-1);
+		buf_index = 0;
+	}
+
+	while (buf_index < bytes_read && buf[buf_index] != '\n')
+	{
+		if (line_length + 1 >= *n)
+		{
+			*n *= 2;
+			*lineptr = realloc(*lineptr, *n);
+			line = *lineptr;
+		if (!line)
+		return (-1);
+		}
+		line[line_length++] = buf[buf_index++];
+	}
+	line[line_length] = '\0';
+	buf_index++;
+
+	if (buf_index >= bytes_read), bytes_read = 0;
+	return (line length);
+}
--- shell.h
+++ shell.h
@@ -12,7 +12,7 @@
 #define MAX_COMMAND_LENGTH 100
 #define MAX_PATH_LENGTH 100
 #define MAX_ARGUMENTS 10
-
+#define MAX_BUFFER_SIZE 1024
 
 /**
  * struct lst_cmd - singly linked list to store command and arguments
@@ -32,5 +32,6 @@ int str_len(char *str);
 int str_cmp(char *str1, char *str2);
 int cmd_exists(char *command);
 int exe(char **cmds, char **av, char **env);
+ssize_t get_line(char **lineptr, size_t *n,     FILE *stream);
 
 #endif
