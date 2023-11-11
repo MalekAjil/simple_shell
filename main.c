f859825154c32aa95a55a7cabd87936e6c2f0039
hassanzali <hassanzali622@gmail.com>
2023-11-10 16:06:00 -0800

    changes

--- main.c
+++ main.c
@@ -3,51 +3,22 @@
 /**
  * main - Entry point
  * @ac: arguments count
- * @av: arguments values
- * @env: environment
+ * @argv: arguments values
+ *
  * Return: 0 (success), 1 (failure)
  */
-int main(int ac, char **av, char **env)
+int main(int ac, char **argv)
 {
-	char *line = NULL;
-	char **cmds = NULL;
+	char *prompt = "($)";
+	char *lineptr;
 	size_t n = 0;
-	ssize_t count = 0;
-	int wcount = 0;
-	char *variable = "NEW_VARIABLE";
-	char *value = "NEW_VALUE";
-	char *path = getenv("PATH");
 
-	printf("%s\n%s\n", path, env[0]);
-	write(1, "($) ", 4);
-	while ((count = get_line(&line, &n, stdin)) != -1)
-	{
-		cmds = str_to_words(line, &wcount);
-		printf("%s\n", line);
-		if (cmds != NULL)
-		{
-			if (str_cmp(cmds[0], "exit"))
-			{
-				exit(0);
-				break;
-			}
-			exe(cmds, av, env);
-		}
-		else
-			write(2, "Error, No command", 17);
-		write(1, "\n($) ", 5);
-		free(cmds);
-	}
-	if (ac != 1 && av == NULL && env == NULL)
-		return (1);
-	if (set_env(variable, value) != -1)
-	{
-		printf("New Enviroment Set");
-	}
-	print_env();
-	if (unset_env("NEW_VARIABLE") != -1)
-	{
-		printf("Enviroment Unset");
-	}
+	(void) ac; (void)argv;
+
+	printf("%s", prompt);
+	printf("%s\n", lineptr);
+	getline(&lineptr, &n, stdin);
+
+	free(lineptr);
 	return (0);
 }
